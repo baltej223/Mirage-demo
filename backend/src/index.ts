c3ce10e023f99310f2a5a38444ba7326ec2add49
baltej223 <baltej963@gmail.com>
2025-11-13 14:42:15 +0530

    Added some more things

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -81,7 +81,19 @@ class PerfMonitor {
 }
 
 const app = express();
+
+// const allowedOrigins = ['http://localhost:3000', 'https'];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error('Not allowed by CORS'));
+//     }
+//   }
+// };
 app.use(cors());
+
 app.use(express.json());
 const PORT = parseInt(process.env.PORT || "3000", 10);
 const perf = new PerfMonitor();
@@ -367,22 +379,6 @@ app.get("/api/refreshCache", async (req, res) => {
     return res.json({ error: "Failed to refresh cache" });
   }
 });
-
-app.get(
-  "/api/leaderboard",
-  perf.middleware("leaderboard"),
-  async (req, res) => {
-    logger.info(`/api/leaderboard`);
-    const teams = await db.collection('mirage-teams').orderBy('points').limit(10).get();
-    const docs = teams.docs.map(x => x.data());
-    res.json({
-      teams: docs.map(x => ({
-        name: x.teamName,
-        points: x.points,
-      })),
-    });
-  }
-) 
 
 app.listen(PORT, '0.0.0.0', async () => {
   logger.info(`${PORT} is now in use`);
